3c9cc65f65b8f81faa3ddae32b35f6346c7148d3
taup3r <79400123+taup3r@users.noreply.github.com>
2026-02-21 22:06:04 +0000

    Refactor Renderer to handle multiple enemies

--- ui/Renderer.js
+++ ui/Renderer.js
@@ -1,21 +1,23 @@
 // ui/Renderer.js
 export default class Renderer {
-  constructor(playerEl, enemyEl, arenaEl) {
+  constructor(playerEl, enemyEls, arenaEl) {
     this.playerEl = playerEl;
-    this.enemyEl = enemyEl;
+    this.enemyEls = enemyEls; // array of DOM elements
     this.arenaEl = arenaEl;
   }
 
-  update(player, enemy) {
+  update(player, enemies) {
     const w = this.arenaEl.clientWidth;
 
+    // Player
     const pxP = (player.position / 100) * w;
-    const pxE = (enemy.position / 100) * w;
-
     this.playerEl.style.transform = `translate(${pxP}px, -50%)`;
-    this.enemyEl.style.transform = `translate(${pxE}px, -50%)`;
 
-    this.playerEl.style.rotate = `${(Math.random() - 0.5) * (100 - player.stability) * 0.05}deg`;
-    this.enemyEl.style.rotate = `${(Math.random() - 0.5) * (100 - enemy.stability) * 0.05}deg`;
+    // Enemies
+    enemies.forEach((enemy, i) => {
+      const pxE = (enemy.position / 100) * w;
+      this.enemyEls[i].style.transform = `translate(${pxE}px, -50%)`;
+      this.enemyEls[i].style.rotate = `${(Math.random() - 0.5) * (100 - enemy.stability) * 0.05}deg`;
+    });
   }
 }
